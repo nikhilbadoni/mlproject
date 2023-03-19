bfa65a743932fc24ebc42dc262aeb0816b86dc19
nikhilbadoni <nikhilbadoni22@gmail.com>
2023-03-19 23:14:21 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 import numpy as np
 import pandas as pd
@@ -21,14 +22,22 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(x_train, y_train)  # Train Model
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+
+            #model.fit(x_train, y_train)  # Train Model
 
             y_train_pred = model.predict(x_train)
 
