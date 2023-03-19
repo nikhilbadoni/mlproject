2bd48d97952936886ed22a233cc09601504c29d3
nikhilbadoni <nikhilbadoni22@gmail.com>
2023-03-19 10:12:30 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -52,8 +52,8 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
 
-data_transformation=DataTransformation()
-data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
         
 
 
